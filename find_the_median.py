2b607b8ec1e434ca77aabe85b5499314d8ac181f
davidcxpardede <davidpardede40@gmail.com>
2023-06-28 10:20:25 +0700

    Find the Median

---
+++ find_the_median.py
@@ -0,0 +1,38 @@
+'''
+The median of a list of numbers is essentially its middle element after sorting. The same number of elements occur after it as before. Given a list of numbers with an odd number of elements, find the median.
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findMedian' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def findMedian(arr):
+    # Write your code here
+    arr.sort()
+    x = math.floor(len(arr)/2)
+    
+    return arr[x]
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = findMedian(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
